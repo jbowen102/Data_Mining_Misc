71ce2e4fb85d4a43ea3fb606a68674d181e1011f
JB (work) <ew15dro6k216@opayq.net>
2024-09-12 20:31:43 -0400

    Create Eclat vertical transformation and get mining up to 2-itemset length working.

--- eclat.py
+++ eclat.py
@@ -10,45 +10,59 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class Eclat(object):
-    def __init__(self, min_sup_abs, transaction_dict):
+    def __init__(self, transaction_dict):
         """Takes in an itemset dict where each value (representing transaction)
         is a set of strings.
         """
-        self.min_sup_count = min_sup_abs
-        self.transaction_db = transaction_dict
+        self.transaction_db_horiz = transaction_dict
 
-        self.C1 = None
-        self.L1 = None
+        self.transaction_db_vert = None  # Will hold transformed db
+        self.min_sup_count = None
+
         self.L = None
 
-    def read_in_C1(self):
-        """Creates self.C1 dict containing all 1-itemsets (no min_sup imposed)
-        from database self.transaction_db.
-        Key: 1-itemset (as frozenset) | Value: support
-        """        
-        transaction_itemsets = [set(val) for val in self.transaction_db.values()]
-
-        self.C1 = dict()
-        # Loop through db transactions and tally 1-itemset supports
-        for itemset in transaction_itemsets:
-            for item in itemset:
-                if self.C1.get(frozenset([item])) is None:
-                    # If item doesn't exist in C_1 dictionary yet, add it.
-                    self.C1[frozenset([item])] = 1
-                else:
-                    self.C1[frozenset([item])] += 1
-
-    def find_L1(self):
-        """Imposes min_support on all 1-itemsets (self.C1) found in
-        self.transaction_db to determine which of them are frequent.
-        Transfers all frequent 1-itemsets to self.L1.
+    def _transform_db(self):
+        """Transforms input "horizontal" database dict into a vertical format.
         """
-        self.read_in_C1()
-        
-        self.L1 = dict()
-        print(Fore.GREEN + Style.BRIGHT + "Pruning C1." + Style.RESET_ALL, end="")
-        for itemset in self.C1:
-            if self.C1[itemset] >= self.min_sup_count:
-                self.L1[itemset] = self.C1[itemset]
-        # print(len(L_1)) # DEBUG: should be 50
-        print(Fore.GREEN + Style.BRIGHT + "..done\n" + Style.RESET_ALL)     
+        self.transaction_db_vert = dict()
+        for transaction_id in self.transaction_db_horiz:
+            for item in self.transaction_db_horiz[transaction_id]:
+                if frozenset(item) in self.transaction_db_vert:
+                    self.transaction_db_vert[frozenset(item)].add(transaction_id)
+                else:
+                    self.transaction_db_vert[frozenset(item)] = set((transaction_id))
+
+
+    def _prune_infrequent(self):
+        """Imposes min_support on all 1-itemsets found in transformed db.
+        """
+        self._transform_db()
+
+        # print(Fore.GREEN + Style.BRIGHT + "Pruning C1." + Style.RESET_ALL, end="")
+        single_items = list(self.transaction_db_vert.keys())
+        for item in single_items:
+            if len(self.transaction_db_vert[item]) < self.min_sup_count:
+                # Remove any 1-itemset from vertical db if its support is too low
+                _ = self.transaction_db_vert.pop(item)
+        # print(Fore.GREEN + Style.BRIGHT + "..done\n" + Style.RESET_ALL)
+
+
+    def find_L_all(self, min_sup_abs):
+        """
+        Mine transformed "vertical" data structure using Eclat algorithm.
+        Find set of all frequent itemsets (of any length) in the database.
+        """
+        self.min_sup_count = min_sup_abs
+        self._transform_db()
+        self._prune_infrequent()
+
+        C_k_dict = dict()
+        for itemset1 in self.transaction_db_vert:
+            for itemset2 in self.transaction_db_vert:
+                if itemset1 == itemset2:
+                    continue
+                intersect = self.transaction_db_vert[itemset1].intersection(self.transaction_db_vert[itemset2])
+                if len(intersect) >= self.min_sup_count:
+                    C_k_dict[itemset1.union(itemset2)] = intersect
+
+        print(C_k_dict)
